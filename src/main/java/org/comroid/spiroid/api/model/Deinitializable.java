b65f7155a629bdaddf71a6ab62769c0303e36ef4
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-27 19:59:17 +0200

    remove bloat

--- src/main/java/org/comroid/spiroid/api/model/Deinitializable.java
+++
@@ -1,9 +0,0 @@
-package org.comroid.spiroid.api.model;
-
-import org.comroid.spiroid.api.exception.DeinitializerException;
-
-import java.io.IOException;
-
-public interface Deinitializable {
-    void deinitialize() throws DeinitializerException, IOException;
-}
--- src/main/java/org/comroid/spiroid/api/model/Initializable.java
+++
@@ -1,9 +0,0 @@
-package org.comroid.spiroid.api.model;
-
-import org.comroid.spiroid.api.exception.InitializerException;
-
-import java.io.IOException;
-
-public interface Initializable {
-    void initialize() throws InitializerException, IOException;
-}
