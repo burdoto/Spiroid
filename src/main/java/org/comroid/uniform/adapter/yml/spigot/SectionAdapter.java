9869d276ef55c7ff4bf610400144101861b57cc2
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-27 20:11:48 +0200

    fix Section Adapter

--- src/main/java/org/comroid/uniform/adapter/yml/spigot/SectionAdapter.java
+++ src/main/java/org/comroid/uniform/adapter/yml/spigot/SectionAdapter.java
@@ -1,28 +1,83 @@
 package org.comroid.uniform.adapter.yml.spigot;
 
 import org.bukkit.configuration.ConfigurationSection;
-import org.comroid.uniform.node.UniObjectNode;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public final class SectionAdapter extends UniObjectNode.Adapter<ConfigurationSection> {
+public final class SectionAdapter implements Map<String, Object> {
+    private final ConfigurationSection section;
+
     SectionAdapter(ConfigurationSection section) {
-        super(section);
+        this.section = section;
+    }
+
+    @Override
+    public int size() {
+        return section.getKeys(false).size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return section.getKeys(false).isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return section.contains(String.valueOf(key));
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return section.getKeys(false)
+                .stream()
+                .map(section::get)
+                .anyMatch(value::equals);
+    }
+
+    @Override
+    public Object get(Object key) {
+        return section.get(String.valueOf(key));
     }
 
     @Override
     public Object put(String name, Object value) {
-        getBaseNode().set(name, value);
+        section.set(name, value);
         return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return put(String.valueOf(key), null);
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends String, ?> m) {
+        m.forEach(this::put);
+    }
+
+    @Override
+    public void clear() {
+        section.getKeys(false).forEach(this::remove);
+    }
+
+    @NotNull
+    @Override
+    public Set<String> keySet() {
+        return section.getKeys(false);
+    }
+
+    @NotNull
+    @Override
+    public Collection<Object> values() {
+        return section.getValues(false).values();
     }
 
     @Override
     public @NotNull Set<Entry<String, Object>> entrySet() {
         final HashSet<Entry<String, Object>> yields = new HashSet<>();
-        getBaseNode().getKeys(false)
-                .forEach(key -> yields.add(new SimpleImmutableEntry<>(key, getBaseNode().get(key))));
+        section.getKeys(false)
+                .forEach(key -> yields.add(new AbstractMap.SimpleImmutableEntry<>(key, section.get(key))));
 
         return yields;
     }
--- src/main/java/org/comroid/uniform/adapter/yml/spigot/SpigotSerializationAdapter.java
+++ src/main/java/org/comroid/uniform/adapter/yml/spigot/SpigotSerializationAdapter.java
@@ -3,26 +3,30 @@ package org.comroid.uniform.adapter.yml.spigot;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.comroid.api.Polyfill;
-import org.comroid.uniform.DataStructureType;
-import org.comroid.uniform.SerializationAdapter;
+import org.comroid.uniform.adapter.AbstractSerializationAdapter;
+import org.comroid.uniform.model.DataStructureType;
+import org.comroid.uniform.model.ValueAdapter;
 import org.comroid.uniform.node.UniArrayNode;
 import org.comroid.uniform.node.UniNode;
 import org.comroid.uniform.node.UniObjectNode;
+import org.comroid.uniform.node.impl.UniObjectNodeImpl;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
-public final class SpigotSerializationAdapter extends SerializationAdapter<ConfigurationSection, ConfigurationSection, ConfigurationSection> {
+public final class SpigotSerializationAdapter extends AbstractSerializationAdapter<ConfigurationSection, ConfigurationSection, ConfigurationSection> {
     public static final SpigotSerializationAdapter INSTANCE = new SpigotSerializationAdapter();
 
     private SpigotSerializationAdapter() {
-        super("application/x-yml", ConfigurationSection.class, ConfigurationSection.class);
+        super("application/x-yml",
+                ConfigurationSection.class, YamlConfiguration::new,
+                ConfigurationSection.class, YamlConfiguration::new);
     }
 
     @Override
-    public DataStructureType<SpigotSerializationAdapter, ConfigurationSection, ? extends ConfigurationSection> typeOfData(String data) {
-        return Polyfill.uncheckedCast(objectType);
+    public DataStructureType<ConfigurationSection, ? extends ConfigurationSection, ? extends UniNode> typeOfData(String data) {
+        return null;
     }
 
     @Override
@@ -42,11 +46,24 @@ public final class SpigotSerializationAdapter extends SerializationAdapter<Confi
     }
 
     @Override
-    public UniObjectNode createUniObjectNode(ConfigurationSection configurationSection) {
-        return new UniObjectNode(this, new SectionAdapter(configurationSection));
+    public UniObjectNode createObjectNode(ConfigurationSection node) {
+        return null;
     }
 
     @Override
+    public UniArrayNode createArrayNode(ConfigurationSection node) {
+        return null;
+    }
+
+    @Override
+    public ValueAdapter<Object, Object> createValueAdapter(Object nodeBase, Predicate<Object> setter) {
+        return null;
+    }
+
+    public UniObjectNode createUniObjectNode(ConfigurationSection configurationSection) {
+        return new UniObjectNodeImpl(this, null, new SectionAdapter(configurationSection));
+    }
+
     public UniArrayNode createUniArrayNode(ConfigurationSection configurationSection) {
         throw new UnsupportedOperationException("Cannot create UniArrayNodes");
     }
